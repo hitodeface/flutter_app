e833b1466e1ec306667f6a242f5d86cddda15cab
[hitodeface] <[ari0703sophia@gmail.com]>
2020-11-08 10:18:22 +0900

    todo app

---
+++ lib/todo_bloc.dart
@@ -0,0 +1,37 @@
+import 'db_provider.dart';
+import 'model/todo.dart';
+import 'dart:async';
+import 'dart:core';
+
+class TodoBloc {
+  final _todoController = StreamController<List<Todo>>();
+  Stream<List<Todo>> get todoStream => _todoController.stream;
+
+  getTodos() async {
+    _todoController.sink.add(await DBProvider.db.getAllTodos());
+  }
+
+  TodoBloc() {
+    getTodos();
+  }
+
+  dispose() {
+    _todoController.close();
+  }
+
+  create(Todo todo) {
+    todo.assignUUID();
+    DBProvider.db.createTodo(todo);
+    getTodos();
+  }
+
+  update(Todo todo) {
+    DBProvider.db.updateTodo(todo);
+    getTodos();
+  }
+
+  delete(String id) {
+    DBProvider.db.deleteTodo(id);
+    getTodos();
+  }
+}
